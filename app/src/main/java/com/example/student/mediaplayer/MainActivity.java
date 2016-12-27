5bd395ae993b7d25ccc59ae1281d123929ba1123
Joseph Roldan <omega6441@gmail.com>
2016-12-26 21:14:34 -0800

    Final temporyary submission

--- app/src/main/java/com/example/student/mediaplayer/MainActivity.java
+++ app/src/main/java/com/example/student/mediaplayer/MainActivity.java
@@ -15,45 +15,87 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     public Button pauseButtonVar, playButtonVar, rewindButtonVar, fastforwardButtonVar, stopButtonVar;
-    private MediaPlayer song1;
-    public TextView currentTimeViewVar, endTimeViewVar, songArtistVar, songTitleVar;
+    public TextView currentTimeViewVar, endTimeViewVar, songArtistVar,songTitleVar;
     public MediaPlayer songPlayer;
     public double endTimeMS, currentTimeMS;
     public Handler myHandler = new Handler();
     private SeekBar mySongBarVar;
-    MediaMetadataRetriever songInfo = new MediaMetadataRetriever();
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MediaMetadataRetriever songInfo = new MediaMetadataRetriever();
         playButtonVar = (Button) findViewById(R.id.play);
         pauseButtonVar = (Button) findViewById(R.id.pause);
-        rewindButtonVar = (Button) findViewById(R.id.backwards);
-        fastforwardButtonVar = (Button) findViewById(R.id.forwards);
+        rewindButtonVar = (Button) findViewById(R.id.backward);
+        fastforwardButtonVar = (Button) findViewById(R.id.forward);
         stopButtonVar = (Button) findViewById(R.id.stop);
-        song1 = MediaPlayer.create(this, R.raw.bargainsinatuxedo);
+
+        currentTimeViewVar = (TextView) findViewById(R.id.currentTimeView);
+        endTimeViewVar = (TextView) findViewById(R.id.endTimeView);
+
+        songTitleVar = (TextView) findViewById(R.id.title);
+        songArtistVar = (TextView) findViewById(R.id.artist);
+
+        songPlayer = MediaPlayer.create(this, R.raw.bargainsinatuxedo);
+
+        endTimeMS = songPlayer.getDuration();
+        int endTimeMin = (int) endTimeMS/1000/60;
+        int endTimeS = (int) (endTimeMS/1000) % 60;
+        endTimeViewVar.setText(endTimeMin+" Min " + endTimeS+ " Sec");
+
+        pauseButtonVar.setEnabled(false);
+        stopButtonVar.setEnabled(false);
+        myHandler.postDelayed(UpdateSongTime, 100);
+        mySongBarVar = (SeekBar) findViewById(R.id.mySongBar);
+        mySongBarVar.setMax((int) endTimeMS);
+
+        Uri mediaPath = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.bargainsinatuxedo);
+        songInfo.setDataSource(this, mediaPath);
+        String songTitle = songInfo.extractMetadata(MediaMetadataRetriever.METADATA_KEY_TITLE);
+        String songArtist = songInfo.extractMetadata(MediaMetadataRetriever.METADATA_KEY_ARTIST);
+        songArtistVar.setText(songArtist);
+        songTitleVar.setText(songTitle);
 
 
-    public void playSong(View view) {
+        mySongBarVar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            int seekTime;
+            @Override
+
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                seekTime = progress;
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                songPlayer.seekTo( seekTime);
+                currentTimeMS = seekTime;
+            }
+        });
+    }
+    public void playSong(View view){
         Toast.makeText(getApplicationContext(), "Playing Song", Toast.LENGTH_SHORT).show();
         pauseButtonVar.setEnabled(true);
         playButtonVar.setEnabled(false);
-        song1.start();
+        songPlayer.start();
         stopButtonVar.setEnabled(true);
     }
-
-    public void pauseSong(View view) {
+    public void pauseSong(View view){
         Toast.makeText(getApplicationContext(), "Pausing Song", Toast.LENGTH_SHORT).show();
         pauseButtonVar.setEnabled(false);
         playButtonVar.setEnabled(true);
         stopButtonVar.setEnabled(true);
-        song1.pause();
+        songPlayer.pause();
     }
-
-    public void stopSong(View view) {
+    public void stopSong(View view){
         Toast.makeText(getApplicationContext(), "Stoping Song", Toast.LENGTH_SHORT).show();
         pauseButtonVar.setEnabled(false);
         playButtonVar.setEnabled(true);
@@ -62,19 +104,31 @@ public class MainActivity extends AppCompatActivity {
         songPlayer.pause();
     }
 
-    public void forward(View view) {
+    public void forward(View view){
         Toast.makeText(getApplicationContext(), "Forward 5s", Toast.LENGTH_SHORT).show();
-        if (currentTimeMS < endTimeMS - 5000) {
+        if(currentTimeMS < endTimeMS - 5000) {
             songPlayer.seekTo((int) currentTimeMS + 5000);
         }
     }
-
-    public void backward(View view) {
+    public void backward(View view){
         Toast.makeText(getApplicationContext(), "Rewind 5s", Toast.LENGTH_SHORT).show();
-        if (currentTimeMS > 5000) {
+        if(currentTimeMS>5000) {
             songPlayer.seekTo((int) currentTimeMS - 5000);
         }
     }
 
+    private Runnable UpdateSongTime = new Runnable(){
+        public void run(){
+            currentTimeMS = songPlayer.getCurrentPosition();
+            int currentTimeMin = (int) currentTimeMS/1000/60;
+            int currentTimeS = (int) (currentTimeMS/1000) % 60;
 
+            currentTimeViewVar.setText(currentTimeMin+" Min " + currentTimeS+ " Sec");
+
+            myHandler.postDelayed(this, 100);
+
+            mySongBarVar.setProgress((int) currentTimeMS);
+
+        }
+    };
 }
